4e17256ad2923e3016c218c29b41fb7f914ddf25
grantfanian <grantfanian@gmail.com>
2024-07-26 00:51:30 +0200

    add restoring one side from two and area; fix to2D

--- src/cz/fanian/shaper/shapes/Triangle.java
+++ src/cz/fanian/shaper/shapes/Triangle.java
@@ -7,6 +7,7 @@ import java.awt.geom.Path2D;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import static java.lang.Math.*;
 
@@ -32,12 +33,11 @@ public class Triangle extends Shape {
      */
     protected static Triangle fromUninitialized(UninitializedShape scratchpad) throws InvalidShapeException, InsufficientDataException {
         var s = scratchpad;
+        if ((s.a == null && s.b == null)
+                || (s.a == null && s.c == null)
+                || (s.b == null && s.c == null))
+            throw new InsufficientDataException("not enough sides info for a triangle");
         if (s.circumference != null) {
-            if ((s.a == null && s.b == null)
-                    || (s.a == null && s.c == null)
-                    || (s.b == null && s.c == null))
-                throw new InsufficientDataException("not enough sides info for a triangle");
-
             if (s.a == null) s.a = s.circumference - s.b - s.c;
             else if (s.b == null) s.b = s.circumference - s.a - s.c;
             else if (s.c == null) s.c = s.circumference - s.a - s.b;
@@ -45,9 +45,18 @@ public class Triangle extends Shape {
                 if (compareWithTolerance(s.a + s.b + s.c, s.circumference) != 0)
                     throw new InvalidShapeException("given triangle perimeter doesn't match sides");
             }
-        } else if (s.a == null || s.b == null || s.c == null) {
-            throw new InvalidShapeException("can't restore sides without circumference");
         } else {
+            if (s.a == null || s.b == null || s.c == null) {
+                if (s.area == null)
+                    throw new InsufficientDataException("can't restore one side without circumference or area");
+
+                if (s.a == null)
+                    s.a = getUnknownSide(s.b, s.c, s.area);
+                else if (s.b == null)
+                    s.b = getUnknownSide(s.a, s.c, s.area);
+                else
+                    s.c = getUnknownSide(s.a, s.b, s.area);
+            }
             s.circumference = s.a + s.b + s.c;
         }
 
@@ -56,11 +65,35 @@ public class Triangle extends Shape {
         double p = s.circumference / 2;
         myArea = sqrt(p * (p - s.a) * (p - s.b) * (p - s.c));
 
-        if (s.area == null) s.area = myArea;
-        else if (compareWithTolerance(s.area, myArea) != 0)
+        if (s.area != null && compareWithTolerance(s.area, myArea) != 0)
             throw new InvalidShapeException("wrong triangle area");
 
         return new Triangle(s.a, s.b, s.c);
+    }
+
+    private static double getUnknownSide(double a, double b, double S) throws InvalidShapeException {
+        // S = (ab * sin C) / 2
+        // sin C = 2S / ab // if out of range, throw (invalid triangle)
+        // c = ab sin C
+        //
+        // c^2 = a^2 + b^2 - 2ab cos C
+        // cos^2 C + sin^2 C = 1
+        //
+        // cos C = (+-) sqrt((ab)^2 - 4S^2) / ab
+        //
+        // c = sqrt(a^2 + b^2 +- 2sqrt((ab)^2 - 4S^2))
+        // ...I guess I'll just leave first valid solution and if all are invalid consider triangle invalid
+        double cosC = sqrt(pow(a * b, 2) - 4 * S * S) / (a * b);
+
+        double squares = a * a + b * b;
+
+        double c = sqrt(squares + 2 * cosC);
+        if (Double.isFinite(c)) return c;
+
+        c = sqrt(squares - 2 * cosC);
+        if (Double.isFinite(c)) return c;
+
+        throw new InvalidShapeException("restored triangle is invalid");
     }
 
     public double getPerimeter() {
@@ -81,14 +114,14 @@ public class Triangle extends Shape {
         //  x^2 + y^2 = b^2
         //  (x + c)^2 + y^2 = a^2
         // }
-        // x^2 + a^2 - (x - c)^2 = b^2
-        // => y = a^2 - (x - c)^2
-        // => x = (b^2 + c^2 - a^2) / 2c
+        // ...
+        // https://stackoverflow.com/a/4002584
+
         Path2D path = new Path2D.Double();
         path.moveTo(0, 0);
         path.lineTo(c, 0);
-        double Cx = (pow(b, 2) + pow(c, 2) - pow(a, 2)) / (2*c);
-        double Cy = (pow(a, 2) - pow((Cx - c), 2)) * 2;
+        double Cx = (a * a - b * b + c * c) / (2 * a);
+        double Cy = sqrt(c * c - Cx * Cx);
         path.lineTo(Cx, Cy);
         path.lineTo(0, 0);
         return path;
